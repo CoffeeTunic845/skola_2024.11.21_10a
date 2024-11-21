ebba8873606a980e7729655f4762f2af4259fd28
CoffeeTunic845 <voice.gleb@gmail.com>
2024-11-21 12:33:26 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,11 +2,14 @@ import os
 os.system('cls')
 print("\n")
 
-
 n = int(input("Ievadiet skaitli n: "))
 
 i = 1
 while i <= n:
-    row = "".join(str(j) for j in range(1, i + 1))
-    print(row.rjust(n))
+    j = 1
+    while j <= i:
+        print(j, end="")
+        j += 1
+    print()
     i += 1
+
