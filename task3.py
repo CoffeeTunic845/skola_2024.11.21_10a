29ea04e4dc797e599ef5d2cea6ac7918e7b77e85
CoffeeTunic845 <voice.gleb@gmail.com>
2024-11-21 12:37:54 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -3,16 +3,11 @@ os.system('cls')
 print("\n")
 
 countdown = 15
-fib0 = 0
-fib1 = 0
-fib2 = 1
+fib1, fib2 = 0, 1
 print(fib1)
-print(fib2)
 
-while countdown > 0:
-    fib0 = fib1 + fib2
-    print(fib0)
-    fib1 = fib2
-    fib2 = fib0
+while countdown > 1:
+    print(fib2)
+    fib1, fib2 = fib2, fib1 + fib2
     countdown -= 1
     
