78375f9c0565135ec0af8822a76eb016338f5fbd
CoffeeTunic845 <voice.gleb@gmail.com>
2024-11-21 11:45:44 +0200

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,30 @@
+import random
+# Metama kaulina attéli
+kaulins —
+l\nl
+l\nl
+l\nl
+l\nl
+•l\nl• •l\nl•
+# Rezultätu sag1abäSana
+rezultati -
+skaits 6 —
+metienu skaits =
+.l\n-----",
+# Generé nejauSu skaitli no 1 lidz 6
+# Metamå kaulioa ar while
+while metienu skaits < 50:
+metiens random. randint(l, 6)
+rezultati. append (metiens )
+if metiens
+6:
+skaits 6 +- 1
+metienu skaits 1
+# Izvada metamä kauliqa attélu
+print(f"Metiens {metienu_skaits}:
+print (kaulins [metiensJ)
+printo
+# Izvade
+print("Visi metienu rezultäti:"
+rezultati)
+uzmests {skaits_6} reizes. ")
